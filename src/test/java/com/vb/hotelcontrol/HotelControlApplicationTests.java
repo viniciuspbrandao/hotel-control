0cd409e50140a15a3ff837347fc58ad6c3bea1ed
Vinicius Brandao <viniciusbrandao@vb4web.com>
2022-10-19 09:35:55 -0300

    Repository creation.

---
+++ src/test/java/com/vb/hotelcontrol/HotelControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vb.hotelcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
